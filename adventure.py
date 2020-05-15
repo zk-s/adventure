6d0c200ba6a87f92114c80ebc1b97fd3efc0f848
zack <zkb6@nau.edu>
2020-05-15 10:44:22 -0700

    less bad. More formatting

--- adventure.py
+++ adventure.py
@@ -1,9 +1,22 @@
 def main():
-    # Players health
-    health = 5
 
-    # Empty inventory to accumulate items in
-    inventory = []
+    class Player:
+        def __init__(self, first_name, health, inventory):
+            self.first_name = first_name
+            self.health = health
+            self.inventory = inventory
+
+        def show_health(self):
+            print(line)
+            print(str(player.health) + ' health')
+            print(line)
+
+        def show_inv(self):
+            print(line)
+            print(player.inventory)
+            print(line)
+    player = Player('Zack', 5, '')
+    player.inventory = []
 
     # list to put items into. this is used to check if a player has already done an action.
     # Such as picking fruit from a cactus and not reprinting the cactus with fruit.
@@ -12,14 +25,15 @@ def main():
     # just some short cuts so I don't have to keep plugging in strings for items
     cactus_fruit = 'Cactus Fruit'
     key = 'Key'
-
+    line = '__________________________________'
     # The choices list I plan to use later on to have some type of weight to actions.
     # like if you punch the bird you'll be shamed for it, or if you didn't go to
     # certain room the game will make tease you for being blind.
-    choices = []
-    options = []
+    # choices = []
+    # options = []
+    idk = "\nI don't know that\n"
 
-    #Ascii art
+    # Ascii art
 
     cactus_with_fruit = '''           
           \  :  /
@@ -58,124 +72,160 @@ def main():
     # or inventory.
 
     player_commands = {
-        'health': health,
-        'inv': inventory,
-        'inventory': inventory
+        'health': player.health,
+        'inv': player.inventory,
+        'inventory': player.inventory
     }
     # Function used to take in the players choice and give them the option to check
     # health and inventory instead of repasting code. Not happy with the global command.
+
     def enter():
         global choice
         choice = input('>')
-        if choice.lower() == 'help':
-            help(inventory, health)
+        if choice.upper() == 'HELP':
+            help()
 
     # A function meant for the player to be able to check there inventory or health
     # at anytime.
 
-    def help(inventory, health):
-        check = input("What do you wish to check? \nHealth/Inventory")
-        print(player_commands[check.lower()])
-        print('Anything else you want to check?')
-        anything_else = input('>')
-        if anything_else in player_commands:
-            print(player_commands[anything_else])
-        if anything_else.lower() == 'yes':
-            help(inventory, health)
+    def help():
+        check = input("What do you wish to check? \n[Health/Inventory]")
+        if check.upper() == 'HEALTH':
+            Player.show_health(player)
+        elif check.upper() == 'INV' or 'INVENTORY':
+            Player.show_inv(player)
+        else:
+            mistype()
+            help()
+
+    def mistype():
+        print(line + idk + line)
+
+    def lb():
+        print(line)
 
     # Staring point. Can travel to Cactus and Vulture from here
+
     def desert():
-        print("You are in the desert. \nYou see a cactus and a vulture")
-        print("Where do you want to go?")
+        print("You are in a desert. \nWhere do you want to go?")
+        print("[Cactus/Vulture]")
         enter()
         if choice.upper() == 'HELP':
             desert()
-        if choice.upper() == 'CACTUS':
-            cactus(inventory,health,items_grabbed)
-        if choice == 'yo':
-            house(inventory,health,items_grabbed)
+        elif choice.upper() == 'CACTUS':
+            cactus()
         elif choice.upper() == 'VULTURE':
-            vulture(inventory, health,items_grabbed)
-
-    # Vulture location. Ideally you want to give the vulture the cactus fruit to
-    # not take damage and not hurt the bird. This is where you can obtained the key from.
-    def vulture(inventory,health,items_grabbed):
-
-        # Checking to see if the player has not collected the key from the vulture
-        # if the player hasn't already collected the key it will run the code
-        # saying the player see a vulture with a key around it's neck.
-        if key not in items_grabbed:
-            print('you walk up to a large vulture standing over bones \n'
-                  'the bird has a key around its neck. What do you wish to do?' )
-
-            # Checking for cactus fruit in inventory
-            if cactus_fruit in inventory:
-                print('Fight / Give fruit')
-                enter()
-            elif cactus_fruit not in inventory:
-                print('Fight')
-                enter()
-            if choice.UPPER() == 'FIGHT':
-                health += -1
-                print('You punch the bird but hurt your hand in the process \n'
-                      'you grab the key from the bird')
-                # adding key both the inventory list and items_grabbed list
-                inventory.append(key)
-                items_grabbed.append(key)
-
-
-            if choice.UPPER() == 'GIVE FRUIT':
-                # Checking for the cactus fruit in inventory
-                if 'Cactus Fruit' in inventory:
-                    print('The bird eats the fruit and you take the key from the bird')
-
-                    # removing cactus fruit from inventory list, adding key to
-                    # inventory list and items_grabbed list.
-                    inventory.remove(cactus_fruit)
-                    inventory.append(key)
-                    items_grabbed.append(key)
-                # if the fruit isn't in inventory this runs
-                elif 'Cactus Fruit' not in inventory:
-                    print('You have no fruit to give')
-        print('Where do you wish to go now?')
-        print('House/Desert')
-        enter()
-        if choice.upper() == 'HOUSE':
-            house(inventory, health, items_grabbed)
-        elif choice.upper() == 'DESERT':
+            vulture()
+        else:
+            mistype()
             desert()
 
     # Stage with a cactus. Player takes the fruit and uses it for a later objective.
-    def cactus(inventory,health, items_grabbed):
+    def cactus():
         print('You walk up to the cactus')
         # Checks for fruit int inventory. If the player had already taken the fruit it will print
         # out a cactus with no fruit.
         if 'Cactus Fruit' not in items_grabbed:
             print(cactus_with_fruit)
-            print("The cactus is tall with fruit blooming!")
-            print('Do you pick the fruit? \n')
+            print("The cactus is tall with fruit blooming! Do you pick up the fruit?")
+            print('[Yes/No]')
             enter()
             if choice.upper() == 'YES':
-                inventory.append(cactus_fruit)
+                print("You pick up the fruit!")
+                player.inventory.append(cactus_fruit)
                 items_grabbed.append(cactus_fruit)
+            elif choice.upper() == 'NO':
+                print("You leave the fruit alone")
 
         # Prints cactus with no fruit
         elif cactus_fruit in items_grabbed:
             print(cactus_without_fruit)
             print("The cactus has no more fruit on it")
-        print("Where do you want to go? \nType Desert or Vulture")
+        print("Where do you want to go? \n[Desert/Vulture]")
         enter()
         if choice.upper() == 'DESERT':
             desert()
         elif choice.upper() == 'VULTURE':
-            vulture(inventory, health, items_grabbed)
+            vulture()
+        else:
+            mistype()
+            cactus()
 
-    def house(inventory, health, items_grabbed):
-        print("its a house")
+    # Vulture location. Ideally you want to give the vulture the cactus fruit to
+    # not take damage and not hurt the bird. This is where you can obtained the key from.
+    def vulture():
+        if key not in items_grabbed:
+            get_key()
+        Player.show_health(player)
+        print('Where do you wish to go now?')
+        print('[House/Desert]')
+        enter()
+        if choice.upper() == 'HOUSE':
+            house()
+        elif choice.upper() == 'DESERT':
+            desert()
+        else:
+            mistype()
+            vulture()
 
-    print("** WELCOME TO ADVENTURE ** ||")
+    def get_key():
+        print('you walk up to a large vulture standing over bones \n'
+              'the bird has a key around its neck. What do you wish to do?')
+        # Checking for cactus fruit in inventory
+        if cactus_fruit in player.inventory:
+            print('[Fight/Give fruit]')
+            enter()
+        elif cactus_fruit not in player.inventory:
+            print('[Fight]')
+            enter()
+        # Checking choice
+        if choice.upper() == 'FIGHT':
+            player.health += -1
+            print('You punch the bird but hurt your hand in the process \n'
+                  'you grab the key from the bird')
+            # adding key both the inventory list and items_grabbed list
+            player.inventory.append(key)
+            items_grabbed.append(key)
+        elif choice.upper() == 'GIVE FRUIT':
+            # Checking for the cactus fruit in inventory
+            if cactus_fruit in player.inventory:
+                print('The bird eats the fruit and you take the key from the bird')
+
+                # removing cactus fruit from inventory list, adding key to
+                # inventory list and items_grabbed list.
+                player.inventory.remove(cactus_fruit)
+                player.inventory.append(key)
+                items_grabbed.append(key)
+            # if the fruit isn't in inventory this runs
+            elif cactus_fruit not in player.inventory:
+                print('You have no fruit to give')
+        elif choice.upper() == 'HELP':
+            get_key()
+        else:
+            mistype()
+            get_key()
+
+    def house():
+        house_options = ['USE KEY', 'USEKEY', 'KEY', 'OPEN', 'UNLOCK']
+        print("You walk up the house and find a locked door")
+        if key in player.inventory:
+            print('[Use Key]')
+            enter()
+            if choice.upper() in house_options:
+                player.inventory.remove(key)
+                inside_house()
+        else:
+            print("Where do you want to go?")
+            print("[Cactus/Vulture]")
+
+    def inside_house():
+        print("You are inside the house")
+        enter()
+    lb()
+    print("** WELCOME TO ADVENTURE **")
+    lb()
     name = input("What's your name? \n > ")
-    print("Welcome " + name + "!")
+    print("Welcome " + name + "!\n")
     desert()
 
 
